b55cdc7bff43116c8df965d702c1e7cbfa91adc9
Kaoru Wada <nagomu@gmail.com>
2020-03-05 20:12:58 +0900

    Add range

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,7 +1,14 @@
-import { lastDayOfMonth } from '../utils';
+import { lastDayOfMonth, range } from '../utils';
 
 describe('lastDayOfMonth', () => {
   it('returns value correctly', () => {
     expect(lastDayOfMonth(2020, 0)).toEqual(31);
+  });
+});
+
+describe('range', () => {
+  it('returns array correctly', () => {
+    const expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    expect(range(0, 10)).toEqual(expected);
   });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,9 @@ export const lastDayOfMonth = (year: number, month: number): number => {
   const first = new Date(year, month, 1);
   return new Date(year, first.getMonth() + 1, 0).getDate();
 };
+
+export const range = (start: number, end: number): number[] => {
+  const list = [];
+  for (let i = start; i <= end; i++) list.push(i);
+  return list;
+};
